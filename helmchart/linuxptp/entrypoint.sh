e9f10fafc7fdadf14831759411058e2a756494d1
ninjab3s <nils@srs.io>
2023-10-09 20:10:11 +0200

    ci: Refactor comments and settings

--- helmchart/linuxptp/entrypoint.sh
+++ helmchart/linuxptp/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-ptp4l -2 -i enp4s0f1 -f /etc/config/linuxptp.cfg -m > /tmp/ptp4l.log 2>&1 & \
-phc2sys -s enp4s0f1 -w -m -R 8 -f /etc/config/linuxptp.cfg > /tmp/phc2sys.log 2>&1
+INTERFACE=enp1s0f0
+ptp4l -2 -i ${INTERFACE} -f /etc/config/linuxptp.cfg -m > /tmp/ptp4l.log 2>&1 & \
+phc2sys -s ${INTERFACE} -w -m -R 8 -f /etc/config/linuxptp.cfg > /tmp/phc2sys.log 2>&1
