0f5eedd923948fe756ff5a221a724273b9af0133
ninjab3s <nils@srs.io>
2023-10-09 20:15:28 +0200

    linuxptp: Define ptp interface name in values.yml

--- helmchart/linuxptp/entrypoint.sh
+++ helmchart/linuxptp/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-INTERFACE=enp1s0f0
+INTERFACE=$1
 ptp4l -2 -i ${INTERFACE} -f /etc/config/linuxptp.cfg -m > /tmp/ptp4l.log 2>&1 & \
 phc2sys -s ${INTERFACE} -w -m -R 8 -f /etc/config/linuxptp.cfg > /tmp/phc2sys.log 2>&1
