61402d1fde2220baec37e53dc4c6d40266d2df62
ninjab3s <nils@srs.io>
2023-11-06 17:48:34 +0100

    helm: Rename charts folder

---
+++ charts/linuxptp/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+INTERFACE=$1
+# ptp4l -2 -i ${INTERFACE} -f /etc/config/linuxptp.cfg -m > /tmp/ptp4l.log 2>&1 & \
+# phc2sys -s ${INTERFACE} -w -m -R 8 -f /etc/config/linuxptp.cfg > /tmp/phc2sys.log 2>&1
+ptp4l -2 -i ${INTERFACE} -f /etc/config/linuxptp.cfg -m  & \
+phc2sys -s ${INTERFACE} -w -m -R 8 -f /etc/config/linuxptp.cfg
--- helmchart/linuxptp/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-INTERFACE=$1
-ptp4l -2 -i ${INTERFACE} -f /etc/config/linuxptp.cfg -m > /tmp/ptp4l.log 2>&1 & \
-phc2sys -s ${INTERFACE} -w -m -R 8 -f /etc/config/linuxptp.cfg > /tmp/phc2sys.log 2>&1
