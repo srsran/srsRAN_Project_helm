8c39c95b73ace8813ed134719ab7b9ad8ac7d5df
ninjab3s <nils@srs.io>
2023-12-03 20:32:45 +0100

    linuxptp image,helm: Add liveness and startup probe

---
+++ images/linuxptp/scripts/liveness-ptp4l.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+upper_limit=$1
+lower_limit=-$upper_limit
+
+# check port state
+state=$(pmc -u -b 0 'GET PORT_DATA_SET' -f /etc/config/linuxptp.cfg | grep -oP 'portState\s+\K\w+' | awk '{print $1}')
+if [ "$state" != "SLAVE" ]; then
+    echo "ptp not established"
+    exit 1
+fi
+
+# check offset
+offset=$(pmc -u -b 0 'GET CURRENT_DATA_SET' -f /etc/config/linuxptp.cfg | grep -oP 'offsetFromMaster\s+\K[-0-9.]+' | awk '{print $1}')
+if (( $(echo "$offset > $upper_limit" |bc -l) )) || (( $(echo "$offset < $lower_limit" |bc -l) )); then
+    echo "offsetFromMaster (${offset}) is not in accepted range"
+    exit 1
+fi
+exit 0
