b513c05d6674dfc97b9b106256c5c55364ad5850
ninjab3s <nils@srs.io>
2024-06-03 21:31:09 +0200

    helm linuxptp: Remove hardcoded update rate for sink

--- charts/linuxptp/resources/entrypoint-phc2sys.sh
+++ charts/linuxptp/resources/entrypoint-phc2sys.sh
@@ -8,7 +8,7 @@
 #
 
 INTERFACE=$1
-phc2sys -s ${INTERFACE} -w -m -R 8 -f /etc/config/linuxptp.cfg  &
+phc2sys -s ${INTERFACE} -w -m -f /etc/config/linuxptp.cfg  &
 phc2sys_pid=$!
 cat "/proc/${phc2sys_pid}/fd/1" > /tmp/phc2sys.stdout &
 liveness-phc2sys.sh /tmp/phc2sys.stdout
