db46384543c160e8bb53ae09d66360c975e1d035
ninjab3s <nils@srs.io>
2024-08-05 12:03:49 +0200

    helm linuxptp: Add ts2phc to linuxptp helm chart

---
+++ charts/linuxptp/resources/entrypoint-ts2phc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Copyright 2021-2023 Software Radio Systems Limited
+#
+# By using this file, you agree to the terms and conditions set
+# forth in the LICENSE file which can be found at the top level of
+# the distribution.
+#
+
+INTERFACE=$1
+ts2phc -c ${INTERFACE} -s nmea -m -f /etc/config/ts2phc.cfg  &
+ts2phc_pid=$!
+cat "/proc/${ts2phc_pid}/fd/1" > /tmp/ts2phc.stdout &
+liveness-ts2phc.sh /tmp/ts2phc.stdout
