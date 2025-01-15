4bc999f2e203b61ead3cc802a6f5eb31e0be7430
Alfredo Sáez <alfredo.saez@srs.io>
2025-01-15 17:27:02 +0100

    all: update copyright header

--- .gitlab/ci/checkers/generate_config.py
+++ .gitlab/ci/checkers/generate_config.py
@@ -97,7 +97,7 @@ def main():
     elif args.mode == 'configMap':
         template = generate_template(config_data)
         full_template = f"""#
-# Copyright 2021-2024 Software Radio Systems Limited
+# Copyright 2021-2025 Software Radio Systems Limited
 #
 # By using this file, you agree to the terms and conditions set
 # forth in the LICENSE file which can be found at the top level of
