1e6a9f8f9d947ec75a03c261252b3d0ff188432e
ninjab3s <nils@srs.io>
2025-02-05 14:59:58 +0100

    helm linuxptp: Improve prints of livenes checks ts2phc,phc2sys

--- images/linuxptp/scripts/liveness-phc2sys.sh
+++ images/linuxptp/scripts/liveness-phc2sys.sh
@@ -7,37 +7,53 @@
 # the distribution.
 #
 
-# This Bash script monitors the ptp4l logfile and checks if
+# This Bash script monitors the phc2sys logfile and checks if
 # the rms value is below a specified threshold (upper_limit). It also checks if
-# the timestamp in the ptp4l logfile increases. It writes a health check
+# the timestamp in the phc2sys logfile increases. It writes a health check
 # file (/tmp/healthcheck) in case the application is running correct. It has a timeout
-# of 10 seconds of not receiving a valid ptp4l messages. If the timeout is reached,
+# of 10 seconds of not receiving a valid NMEA messages. If the timeout is reached,
 # the health check file is removed and the applications terminates.
 
 phc2sys_output_file=$1
 upper_limit=120   # max rms
-lower_limit=-120  # max rms
+lower_limit=-120  # min rms
 timeout=10        # timeout in seconds
 last_timestamp=0
 counter=0
 
 while true; do
     last_line=$(tail -n 1 "$phc2sys_output_file")
-    if [[ $last_line == *"CLOCK_REALTIME phc"* ]]; then
+
+    if [[ -z $last_line ]]; then
+        echo "Warning: No new data found in $phc2sys_output_file. Check if the system is working properly."
+        ((counter++))
+    elif [[ $last_line == *"CLOCK_REALTIME phc"* ]]; then
         timestamp=$(echo "$last_line" | awk -F'[][]' '{print $2}' | sed 's/://' | cut -d'.' -f1)
         last_value=$(echo "$last_line" | awk '/CLOCK_REALTIME phc offset/ {print $5}')
-        if [ "$last_value" -lt "$upper_limit" ] && [ "$last_value" -gt "$lower_limit" ] && [ "$timestamp" -gt "$last_timestamp" ]; then
-            touch /tmp/phc2sys-healthy
-            last_timestamp=$timestamp
-            counter=0
+
+        # Validate the timestamp and last_value
+        if [[ -n $timestamp && $timestamp =~ ^[0-9]+$ ]] && [[ -n $last_value && $last_value =~ ^-?[0-9]+$ ]]; then
+            if [ "$last_value" -lt "$upper_limit" ] && [ "$last_value" -gt "$lower_limit" ] && [ "$timestamp" -gt "$last_timestamp" ]; then
+                touch /tmp/phc2sys-healthy
+                last_timestamp=$timestamp
+                counter=0
+            else
+                echo "Warning: Invalid timestamp or offset value detected in log entry."
+                echo "Warning: Last line: $last_line"
+                ((counter++))
+            fi
         else
+            echo "Warning: Invalid timestamp or offset value detected in log entry."
+            echo "Warning: Last line: $last_line"
             ((counter++))
         fi
     else
+        echo "Warning: No valid entry found in the phc2sys log."
         ((counter++))
     fi
 
     if [ "$counter" -gt "$timeout" ]; then
+        echo "Timeout reached: No valid data received for $timeout seconds. Marking system as unhealthy."
         rm -f /tmp/phc2sys-healthy
     fi
     sleep 1
--- images/linuxptp/scripts/liveness-ts2phc.sh
+++ images/linuxptp/scripts/liveness-ts2phc.sh
@@ -7,11 +7,11 @@
 # the distribution.
 #
 
-# This Bash script monitors the ptp4l logfile and checks if
+# This Bash script monitors the ts2phc logfile and checks if
 # the rms value is below a specified threshold (upper_limit). It also checks if
-# the timestamp in the ptp4l logfile increases. It writes a health check
+# the timestamp in the ts2phc logfile increases. It writes a health check
 # file (/tmp/healthcheck) in case the application is running correct. It has a timeout
-# of 10 seconds of not receiving a valid ptp4l messages. If the timeout is reached,
+# of 10 seconds of not receiving a valid NMEA messages. If the timeout is reached,
 # the health check file is removed and the applications terminates.
 
 ts2phc_output_file=$1
@@ -23,22 +23,38 @@ counter=0
 
 while true; do
     last_line=$(grep 'offset' "$ts2phc_output_file" | tail -n 1)
-    if [[ $last_line == *"s2"* ]]; then
+
+    if [[ -z $last_line ]]; then
+        echo "Warning: No valid offset data found. Check if the GPS antenna is connected properly."
+        ((counter++))
+    elif [[ $last_line == *"s2"* ]]; then
         timestamp=$(echo "$last_line" | awk -F'[][]' '{print $2}')
         last_rms_value=$(echo "$last_line" | sed -n 's/.*offset\s*\([-0-9]*\)\s*s2.*/\1/p' | awk '{print int($1)}')
-        if [ "$last_rms_value" -lt "$upper_rms_limit" ] && [ "$last_rms_value" -gt "$lower_rms_limit" ] && (( $(echo "$timestamp > $last_timestamp" | bc -l) )); then
-            touch /tmp/ts2phc-healthy
-            last_timestamp=$timestamp
-            counter=0
+
+        # Check if both timestamp and last_rms_value are valid integers
+        if [[ -n $timestamp && $timestamp =~ ^[0-9]+$ ]] && [[ -n $last_rms_value && $last_rms_value =~ ^-?[0-9]+$ ]]; then
+            if [ "$last_rms_value" -lt "$upper_rms_limit" ] && [ "$last_rms_value" -gt "$lower_rms_limit" ] && (( $(echo "$timestamp > $last_timestamp" | bc -l) )); then
+                touch /tmp/ts2phc-healthy
+                last_timestamp=$timestamp
+                counter=0
+            else
+                echo "Warning: Invalid timestamp or offset value detected."
+                echo "Warning: Last line: $last_line"
+                ((counter++))
+            fi
         else
+            echo "Warning: Invalid timestamp or offset value detected."
+            echo "Warning: Last line: $last_line"
             ((counter++))
         fi
     else
+        echo "Warning: Last entry found in ts2phc output is invalid."
+        echo "Warning: Last line: $last_line"
         ((counter++))
     fi
 
     if [ "$counter" -gt "$timeout" ]; then
         rm -f /tmp/ts2phc-healthy
     fi
-    sleep 1
+    sleep 2
 done
